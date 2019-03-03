9d90f976edbe7dc3029f9e8564930f93887de229
martin <martin@placka.placka.local>
2019-03-03 18:48:09 +0100

    pascal triangle

--- cv2/src/main/java/com/mycompany/cv2/Cv2.java
+++ cv2/src/main/java/com/mycompany/cv2/Cv2.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.cv2;
 
+import ij.ImagePlus;
+import ij.io.FileSaver;
+import ij.process.ColorProcessor;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 /**
@@ -128,6 +132,74 @@ public class Cv2 {
         return finalList;
     }
     
+    public static void pascalTriangle(int h, int d){
+        if(d>256) d=256;
+        int w = 2*h+1;
+        
+        BigDecimal[][] field = new BigDecimal[w][h];
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                field[x][y]=new BigDecimal(0);
+            }
+        }
+        
+        field[w/2][0]=new BigDecimal(1);
+        
+        for (int y = 1; y < h; y++)
+        {
+            for(int x = w/2 - y; x <= w/2 + y; x += 2)
+            {
+                field[x][y] = field[x-1][y-1].add(field[x+1][y-1]);
+            }
+            for(int x = w/2 - y+1; x <= w/2 + y-1; x += 2)
+            {
+                field[x][y] = new BigDecimal(2*d);
+            }
+        }
+        
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                System.out.print(" "+field[x][y]+" ");
+            }
+            System.out.print("\n");
+        }
+        
+        ColorProcessor ip = new ColorProcessor(w, h);
+        
+        
+        int[] pixelRGB = new int[3];
+        
+        pixelRGB[0] = 0;
+        pixelRGB[1] = 0;
+        pixelRGB[2] = 0;
+        
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                pixelRGB[0] = 0;
+                
+                pixelRGB[2] = (int)((255/d)*field[x][y].remainder(new BigDecimal(d)).intValue());
+                
+                if(pixelRGB[2] == 0 && !field[x][y].equals(new BigDecimal(2*d)) && x >= w/2 - y && x <= w/2 + y){
+                    pixelRGB[0] = 255;
+                }
+                
+                
+                ip.putPixel(x, y, pixelRGB);
+            }
+        }
+        ImagePlus img = new ImagePlus("image", ip);
+        //img.show();
+        FileSaver fs = new FileSaver(img);
+        fs.saveAsPng("pascalTriangle.png");
+    }
+    
+    
     public static void main(String [] args){
         ArrayList list = new ArrayList();
         list.add('A');
@@ -139,6 +211,6 @@ public class Cv2 {
         System.out.println("variace s opakovanim: "+ variationsWithRepetition(list, new ArrayList(), 2));
         System.out.println("kombinace: "+ combinations(list, new ArrayList(), 2));
         System.out.println("kombinace s opakovanim: : "+ combinationsWithRepetition(list, new ArrayList(), 2));
-        
+        pascalTriangle(500, 5);
     }
 }
