893b6c371cafc2032c4d2a906ec3b10b925f0942
martin <martin@placka.placka.local>
2019-04-08 12:40:52 +0200

    cv8start

---
+++ cv8/src/main/java/com/mycompany/cv8/Cv8.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cv8;
+
+/**
+ *
+ * @author xmejzli2
+ */
+public class Cv8 {
+    
+    public static SVG drawNgon(SVG svg, double[][] points){
+
+        
+        for(int i = 0; i < points.length; i++){
+            svg.line(points[i][0], points[i][1], points[(i+1)%points.length][0], points[(i+1)%points.length][1], 0, 0, 0);
+        }
+        return svg;
+    }
+    
+    public static void A1(double side, int numOfIterations){
+        int size = 1000;
+        SVG svg = new SVG(size, size, "A1");
+        
+        double centerX = 500.0;
+        double centerY = 500.0;
+        
+        MyMatrixUtils myutils = new MyMatrixUtils();
+        
+        double[][] coordinates1 = new double[][]{
+            {0}, {0}, {1}
+        };
+        
+        double[][] coordinates2 = new double[][]{
+            {0}, {-side}, {1}
+        };
+        
+        double[][] coordinates3 = new double[][]{
+            {-side}, {-side}, {1}
+        };
+        
+        double[][] coordinates4 = new double[][]{
+            {-side}, {0}, {1}
+        };
+        
+        for(int i = 0; i < numOfIterations; i++){
+            svg = drawNgon(
+                svg, 
+                new double[][]{
+                    {coordinates1[0][0] + centerX, coordinates1[1][0] + centerY},
+                    {coordinates2[0][0] + centerX, coordinates2[1][0] + centerY},
+                    {coordinates3[0][0] + centerX, coordinates3[1][0] + centerY},
+                    {coordinates4[0][0] + centerX, coordinates4[1][0] + centerY}
+                }
+            );
+
+            double[][] rotation = myutils.rotation(20);
+            double[][] scaling = myutils.scaling(new double[]{1.1, 1.1});
+            double[][] translation = myutils.translation(new double[]{5, 10});
+
+            double[][][] composition = new double[][][]{rotation, scaling, translation};
+
+
+            double[][] finalMatrix = myutils.combine(composition);
+            //myutils.printMatrix(finalMatrix);
+
+            coordinates1 = myutils.multiplyMatrices(finalMatrix, coordinates1);
+            coordinates2 = myutils.multiplyMatrices(finalMatrix, coordinates2);
+            coordinates3 = myutils.multiplyMatrices(finalMatrix, coordinates3);
+            coordinates4 = myutils.multiplyMatrices(finalMatrix, coordinates4);
+        }
+        
+        svg.save();
+    }
+    
+    public static void main(String [] args){
+        MyMatrixUtils utils = new MyMatrixUtils();
+        //utils.printMatrixMultiplicationTest();
+        A1(50, 11);
+        
+    }
+}
---
+++ cv8/src/main/java/com/mycompany/cv8/MyMatrixUtils.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cv8;
+
+/**
+ *
+ * @author xmejzli2
+ */
+public class MyMatrixUtils {
+    public static double[][] multiplyMatrices(double[][] A, double B[][]){
+        double[][] C = new double[A.length][B[0].length];
+        
+        for(int i = 0; i < A.length; i++){
+            for(int k = 0; k < B[0].length; k++){
+                C[i][k] = 0;
+                for(int j = 0; j < A[0].length; j++){
+                    C[i][k] += (A[i][j]*B[j][k]);
+                }
+            }
+        }
+        return C;
+    }
+    
+    public static void printMatrixMultiplicationTest(){
+        double[][] A = new double[][]{{1,2,3},{4,5,6}};
+        double[][] B = new double[][]{{-1,2},{4,5},{7,8}};
+        
+        double[][] C = multiplyMatrices(A,B);
+        
+        for(int i = 0; i < C.length; i++){
+            for(int j = 0; j < C[0].length; j++){
+                System.out.print(C[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static void printMatrix(double[][] C){
+        for(int i = 0; i < C.length; i++){
+            for(int j = 0; j < C[0].length; j++){
+                System.out.print(C[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static double[][] rotation(double angle){
+        double a = angle * 2*Math.PI/360;
+        return new double[][]{
+            {Math.cos(a), -Math.sin(a), 0}, 
+            {Math.sin(a),  Math.cos(a), 0}, 
+            {0,           0,            1}
+        };
+    }
+    
+    public static double[][] rotate(double[][] A, double angle){
+        double[][] B = rotation(angle);
+        return multiplyMatrices(A,B);
+    }
+    
+    public static double[][] translation(double[] t){
+        return new double[][]{
+            {1, 0, t[0]}, 
+            {0, 1, t[1]}, 
+            {0, 0, 1   }
+        };
+    }
+    
+    public static double[][] translate(double[][] A, double[] t){
+        double[][] B = translation(t);
+        return multiplyMatrices(A,B);
+    }
+    
+    public static double[][] scaling(double[] s){
+        return new double[][]{
+            {s[0], 0, 0}, 
+            {0, s[1], 0}, 
+            {0, 0, 1   }
+        };
+    }
+    
+    public static double[][] scale(double[][] A, double[] s){
+        double[][] B = scaling(s);
+        return multiplyMatrices(A,B);
+    }
+    
+    public static double[][] combine(double[][][] transformations){
+        double[][] result = new double[][]{
+            {1, 0, 0},
+            {0, 1, 0},
+            {0, 0, 1}
+        };
+        for(int i = 0; i < transformations.length; i++){
+            result = multiplyMatrices(transformations[i], result);
+        }
+        return result;
+    }
+}
